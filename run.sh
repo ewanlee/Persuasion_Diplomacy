7d50b31e34b1dc51e169bea76f5ceb6244ea48f6
sam-paech <152407511+sam-paech@users.noreply.github.com>
2025-06-22 14:41:29 +1000

    add resuming support + critical state analysis mode

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 python3 lm_game.py \
     --max_year 1901 \
     --num_negotiation_rounds 0 \
-    --models "openrouter-google/gemini-2.5-flash-preview-05-20, openrouter-google/gemini-2.5-flash-preview-05-20, openrouter-google/gemini-2.5-flash-preview-05-20, openrouter-google/gemini-2.5-flash-preview-05-20, openrouter-google/gemini-2.5-flash-preview-05-20, openrouter-google/gemini-2.5-flash-preview-05-20, openrouter-google/gemini-2.5-flash-preview-05-20" \
+    --models "openrouter-google/gemini-2.5-flash-lite-preview-06-17, openrouter-google/gemini-2.5-flash-lite-preview-06-17, openrouter-google/gemini-2.5-flash-lite-preview-06-17, openrouter-google/gemini-2.5-flash-lite-preview-06-17, openrouter-google/gemini-2.5-flash-lite-preview-06-17, openrouter-google/gemini-2.5-flash-lite-preview-06-17, openrouter-google/gemini-2.5-flash-lite-preview-06-17" \
     --max_tokens_per_model 16000,16000,16000,16000,16000,16000,16000
